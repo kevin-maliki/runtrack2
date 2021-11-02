fcc10b39645264a0afcfad38799ec61daf2407f2
Kevin MALIKI <91872212+kevin-maliki@users.noreply.github.com>
2021-11-02 13:16:04 +0100

    update index job02

---
+++ jour01/job02/index.php
@@ -0,0 +1,7 @@
+<?php
+$str= "hello"; // premier str pour afficher hello
+$str2= "LaPlateforme"; // deuxième str pour afficher LaPlateforme
+$str3= "!"; // troisième str pour afficher le "!"
+echo "$str $str2 $str3"; // echo pour afficher les str
+echo " <br>$str $str2 $str3"; // echo pour afficher les str
+?>
