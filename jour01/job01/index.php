816a5200f2b73e5be39f7c3086270571e61637a5
Kevin MALIKI <91872212+kevin-maliki@users.noreply.github.com>
2021-11-02 12:49:14 +0100

    fin job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,13 @@
+<?php
+$str= "LaPlateforme"; // Variable str "laplateforme"
+echo $str; // echo permet d'afficher la variable
+$str2= "Vive"; // Variable str2 "Vive"
+echo $str2; // echo permet d'afficher la variable
+$str3= "!"; // Variable str3 "!"
+echo $str3; // echo permet d'afficher la variable
+$val= 4;
+echo $val;
+$myBool= false;
+echo $myBool;
+?>
+
