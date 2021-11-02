8bb6386ab5cc93540fc50a057c63078c0a9a450e
Kevin MALIKI <91872212+kevin-maliki@users.noreply.github.com>
2021-11-02 14:45:20 +0100

    Update index.php

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -3,7 +3,7 @@ $str= "LaPlateforme"; // Variable str "laplateforme"
 $str2= "Vive"; // Variable str2 "Vive"
 $str3= "!"; // Variable str3 "!"
 echo $str2." ".$str . $str3; // echo permet d'afficher la variable
-$val= 4;
+$val= 6+4;
 echo $val;
 $myBool= false;
 echo $myBool;
