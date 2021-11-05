4ba5f73475681b38a9f08ed973e10bcbcd0d4e43
Kevin MALIKI <91872212+kevin-maliki@users.noreply.github.com>
2021-11-05 14:23:51 +0100

    Create index.php

---
+++ jour04/job01/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GET</title>
+</head>
+<body>
+    <form action="./index.php" method="get">
+  <div>
+    <label for="name">Nom :</label>
+    <input type="text" id="name" name="user_name" />
+  <div>
+  <div>
+    <label for="mail">E-mail :</label>
+    <input type="email" id="mail" name="user_email" />
+  </div>
+    <input type="submit" value="Valide" />
+  </form>
+</body>
+</html>
