507bb3560a5151cd2b59d020ea01043b149789ad
Kevin MALIKI <91872212+kevin-maliki@users.noreply.github.com>
2021-11-08 10:29:51 +0100

    Create index.php

---
+++ jour05/job01/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>job01</title>
+</head>
+<body>
+<?php
+$hello = "Hello LaPlateforme";
+$couleur = "black";
+function afficher($hello, $couleur) {
+print ("<font color=\"$couleur\"> $hello </font>");
+}
+
+afficher($hello, $couleur='black')
+?>
+    
+</body>
+</html>
